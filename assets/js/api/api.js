c63d9657debc8dd855e762656bf44b421d750d65
filipetenedini <tenedinifilipe@gmail.com>
2023-01-19 00:24:17 -0300

    keep logged in request ok

--- assets/js/api/api.js
+++ assets/js/api/api.js
@@ -2,23 +2,49 @@ import { getData } from '../functions/functions.js';
 
 const BASE = "https://mock-api.driven.com.br/api/v6/uol";
 
+
+let obj;
+
 const apiFunc = {
 
     userLogin: () => {
-        const obj = getData();
-
-        const response = axios.post(`${BASE}/participants`, obj);
-
-        response.then( () => apiFunc.viewMsgs() )
-        response.catch( () => apiFunc.userLogin() )
+        obj = getData();
+        
+        axios
+        .post(`${BASE}/participants`, obj)
+        .then( () => apiFunc.viewMsgs() )
+        .catch( (e) => console.log(e) ); //descobrir qual tipo de erro e tratar
     },
 
+    keepLogedIn: () => {
+        axios
+        .post(`${BASE}/status`, obj)
+        .then(()=>console.log('manteu'))
+        .catch();
+    },
+
+    viewMsgs: () => {
+        axios
+        .get(`${BASE}/messages`)
+        .then(seeMsgSuccess)
+        .catch(seeMsgFail);
+    },
 
 }
 
+function seeMsgSuccess(response){
+    console.log(response)
+}
+
+function seeMsgFail(response){
+    console.log(response)
+}
+
+const holdLogin = () => setInterval(apiFunc.keepLogedIn, 5000);
 
 
 
 apiFunc.userLogin();
+holdLogin();
 
 export {};
